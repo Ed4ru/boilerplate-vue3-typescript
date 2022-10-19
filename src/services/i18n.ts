50bf3a0df250e412b7d71377f251e8a09828bb5f
Arnaud <arnaud@lille-esport.gg>
2022-10-19 11:01:47 +0200

    improved i18n management

--- src/services/i18n.ts
+++ src/services/i18n.ts
@@ -1,10 +1,11 @@
 import { createI18n } from 'vue-i18n';
+import { merge } from 'lodash';
+
 import global from '@/assets/i18n/global.json';
+import navigation from '@/assets/i18n/navigation.json';
 
 export const i18n = createI18n({
   legacy: false,
   locale: 'en',
-  messages: {
-    ...global
-  }
+  messages: merge(global, navigation)
 });

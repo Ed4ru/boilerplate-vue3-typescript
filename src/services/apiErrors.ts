a460154bd9f6f7ecfe4c8069f6f126baf544f5ba
Arnaud <arnaud@lille-esport.gg>
2022-10-24 16:10:16 +0200

    commented the file, because it's useless for now

--- src/services/apiErrors.ts
+++ src/services/apiErrors.ts
@@ -1,41 +1,45 @@
-import { set } from 'lodash';
-import { reactive } from 'vue';
-import { AxiosError } from 'axios';
+// ==================================
+/* THIS FILE ISN'T USEFUL FOR NOW IT WILL BE USED FOR THE FULLSTACK STARTER PACK */
+// ==================================
 
-export const apiErrors: Record<string, Record<string, unknown>> = reactive({});
+// import { set } from 'lodash';
+// import { reactive } from 'vue';
+// import { AxiosError } from 'axios';
 
-type ApiError = {
-  field?: string;
-  message: string;
-};
+// export const apiErrors: Record<string, Record<string, unknown>> = reactive({});
 
-const setApiErrors = (name: string, errors: ApiError[]) => {
-  errors.forEach((error) => {
-    const field =
-      !error.field || error.field === 'base' ? 'global' : error.field;
-    set(apiErrors, `${name}.${field}`, error.message);
-  });
-};
+// type ApiError = {
+//   field?: string;
+//   message: string;
+// };
 
-export const handleApiError = (
-  name: string,
-  apiError: AxiosError<Record<string, ApiError[]>>
-) => {
-  switch (apiError.request.code) {
-    case 0:
-      console.error('Network Error');
-      break;
-    case 403:
-    case 404:
-    case 500:
-      console.error(`${apiError.request.code} Error`);
-      break;
-    case 400:
-      if (apiError.response?.data?.errors) {
-        setApiErrors(name, apiError.response.data.errors);
-      } else {
-        console.error('Undefined error');
-      }
-      break;
-  }
-};
+// const setApiErrors = (name: string, errors: ApiError[]) => {
+//   errors.forEach((error) => {
+//     const field =
+//       !error.field || error.field === 'base' ? 'global' : error.field;
+//     set(apiErrors, `${name}.${field}`, error.message);
+//   });
+// };
+
+// export const handleApiError = (
+//   name: string,
+//   apiError: AxiosError<Record<string, ApiError[]>>
+// ) => {
+//   switch (apiError.request.code) {
+//     case 0:
+//       console.error('Network Error');
+//       break;
+//     case 403:
+//     case 404:
+//     case 500:
+//       console.error(`${apiError.request.code} Error`);
+//       break;
+//     case 400:
+//       if (apiError.response?.data?.errors) {
+//         setApiErrors(name, apiError.response.data.errors);
+//       } else {
+//         console.error('Undefined error');
+//       }
+//       break;
+//   }
+// };

2019cfac7260d9d1ac164878e10da3ddec7d69b3
Arnaud <89442525+Ed4ru@users.noreply.github.com>
2022-10-31 22:59:24 +0100

    Improved errors handling (#7)

--- src/services/api.ts
+++ src/services/api.ts
@@ -16,52 +16,28 @@ export const apiWrapper = {
   responseBody: (response: AxiosResponse) => response.data,
 
   get: async function (url: string, params = {}) {
-    try {
-      const response: AxiosResponse = await this.instance.get(url, { params });
-      return this.responseBody(response);
-    } catch (error) {
-      if (axios.isAxiosError(error)) throw error;
-      throw new Error('Axios GET error : error type is not AxiosError');
-    }
+    const response: AxiosResponse = await this.instance.get(url, { params });
+    return this.responseBody(response);
   },
 
   post: async function (url: string, data = {}, config?: AxiosRequestConfig) {
-    try {
-      const response: AxiosResponse = await this.instance.post(
-        url,
-        data,
-        config
-      );
-      return this.responseBody(response);
-    } catch (error) {
-      if (axios.isAxiosError(error)) throw error;
-      throw new Error('Axios POST error : error type is not AxiosError');
-    }
+    const response: AxiosResponse = await this.instance.post(url, data, config);
+    return this.responseBody(response);
   },
 
   patch: async function (url: string, data = {}, config?: AxiosRequestConfig) {
-    try {
-      const response: AxiosResponse = await this.instance.patch(
-        url,
-        data,
-        config
-      );
-      return this.responseBody(response);
-    } catch (error) {
-      if (axios.isAxiosError(error)) throw error;
-      throw new Error('Axios PATCH error : error type is not AxiosError');
-    }
+    const response: AxiosResponse = await this.instance.patch(
+      url,
+      data,
+      config
+    );
+    return this.responseBody(response);
   },
 
   delete: async function (url: string, params = {}) {
-    try {
-      const response: AxiosResponse = await this.instance.delete(url, {
-        params
-      });
-      return this.responseBody(response);
-    } catch (error) {
-      if (axios.isAxiosError(error)) throw error;
-      throw new Error('Axios DELETE error : error type is not AxiosError');
-    }
+    const response: AxiosResponse = await this.instance.delete(url, {
+      params
+    });
+    return this.responseBody(response);
   }
 };

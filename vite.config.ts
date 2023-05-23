5442eed50160a3211a8d8f75b412b47b2d250e58
Arnaud <89442525+Ed4ru@users.noreply.github.com>
2023-05-23 04:48:51 +0200

    better vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,27 @@
 import { defineConfig, loadEnv } from 'vite';
+import vueI18n from '@intlify/unplugin-vue-i18n/vite';
 import vue from '@vitejs/plugin-vue';
-import vueI18n from '@intlify/vite-plugin-vue-i18n';
-import path from 'path';
+import eslintPlugin from 'vite-plugin-eslint';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-  return {
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), '') };
+
+  return defineConfig({
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, './src'),
-        '#': path.resolve(__dirname, './src/components')
+        '@': resolve(__dirname, './src'),
+        '#': resolve(__dirname, './src/components')
       }
     },
-    plugins: [vue({ reactivityTransform: true }), vueI18n()],
+    plugins: [
+      vue({ reactivityTransform: true }),
+      vueI18n({ include: resolve(__dirname, './src/assets/i18n/**') }),
+      eslintPlugin()
+    ],
     server: {
-      port: parseInt(env.VITE_PORT) || 3333
+      port: parseInt(process.env.VITE_PORT) || 3333
     }
-  };
-});
+  });
+};
